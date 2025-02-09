aa0b9fccbcf9c60b23d326f5c72c26dcf7ebec91
HelKornilova <elenatutkina@gmail.com>
2025-02-09 11:14:06 +0300

    Add files via upload

---
+++ Lesson_1.py
@@ -0,0 +1,24 @@
+#Задание 1
+date = input('Введите дату: ')
+note = input('Введите задачу: ')
+print(date, note)
+
+#Задание 2
+date_1 = input('Введите дату: ')
+note_1 = input('Введите задачу: ')
+date_2 = input('Введите дату: ')
+note_2 = input('Введите задачу: ')
+date_3 = input('Введите дату: ')
+note_3 = input('Введите задачу: ')
+print(date_1, note_1)
+print(date_2, note_2)
+print(date_3, note_3)
+
+#Задание 3
+date_1 = input('Введите дату: ')
+note_1 = input('Введите задачу: ')
+date_2 = input('Введите дату: ')
+note_2 = input('Введите задачу: ')
+date_3 = input('Введите дату: ')
+note_3 = input('Введите задачу: ')
+notes = {date_1: note_1, date_2: note_2, date_3: note_3}
